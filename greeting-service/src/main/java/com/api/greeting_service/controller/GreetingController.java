b801e1908d3eebae92d571b371f2450ced51d11f
Sam <s4muelrp@gmail.com>
2025-09-01 21:44:51 -0300

    configs

---
+++ greeting-service/src/main/java/com/api/greeting_service/controller/GreetingController.java
@@ -0,0 +1,34 @@
+package com.api.greeting_service.controller;
+
+import com.api.greeting_service.config.GreetingConfiguration;
+import com.api.greeting_service.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "%s, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private GreetingConfiguration configuration;
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "")
+            String name){
+
+        if(name.isEmpty()) name = configuration.defaultValue();
+        return new Greeting(
+                counter.incrementAndGet(), String.format(template,
+                configuration.greeting(),
+                name)
+        );
+
+    }
+}
---
+++ greeting-service/src/main/java/com/api/greeting_service/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.api.greeting_service.model;
+
+public record Greeting(long id, String content) {
+}
